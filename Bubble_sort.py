46f41721f7a29d4f05c1c4c1a49a901dfa48c97f
naru2001 <50087796+naru2001@users.noreply.github.com>
2019-07-17 20:59:38 +0900

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,21 @@
+def main(n,a):
+
+    cnt=0
+    for i in range(n-1):
+        id=i
+        mini=a[i]
+        ch=False
+        for j in range(i,n):
+            if mini>a[j]:
+                mini=a[j]
+                id=j
+                ch=True
+        if ch:
+            a[i],a[id]=a[id],a[i]
+            cnt+=1
+    print(*a)
+    print(cnt)
+
+n=int(input())
+a=list(map(int,input().split()))
+main(n,a)
