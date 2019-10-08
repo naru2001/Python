38bb010ec3b4b14e7c484609dbd8a957f33e4fa0
naru2001 <50087796+naru2001@users.noreply.github.com>
2019-10-08 21:40:44 +0900

    Add files via upload

---
+++ BIT.py
@@ -0,0 +1,17 @@
+class Binary_Indexed_Tree:
+
+    def __init__(self,n):
+        self.size=n
+        self.graph=[0]*(n+1)
+
+    def partially_sum(self,i):
+        res=0
+        while i>0:
+            res+=self.graph[i]
+            i-=i&-i
+        return res
+
+    def add_element(self,i,x):
+        while self.size>=i:
+            self.graph[i]+=x
+            i+=i&-i
