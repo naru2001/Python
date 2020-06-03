765c82c7a37efb58a09f4c43edb19a873a7cf66d
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-06-03 22:48:20 +0900

    O(n)で1!~n!

--- fast_factorial.py
+++ fast_factorial.py
@@ -1,15 +1,6 @@
-from sys import setrecursionlimit
-
-setrecursionlimit(10**9)
-
-def f1(n):
-    if a[n]!=0:
-        return a[n]
-    return n*f1(n-1)%(10**9+7)
-
-a=[0]*1000000
-a[0],a[1]=1,1
-n=int(input())
-for i in range(1,n+1):
-    a[i]=f1(i)
-print(a[n])
+def FF(n,p=10**9+7): #1~nまでの階乗 mod p
+    ret=[1]+[i for i in range(1,n+1)]
+    for i in range(3,n+1):
+        ret[i]*=ret[i-1]
+        ret[i]%=p
+    return ret
