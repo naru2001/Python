cb1056bf01185a88faf6c12d4b9afd2be9e2ce51
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-07-04 00:11:10 +0900

    おそい

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,11 +1,9 @@
-n=int(input())
-a=list(map(int,input().split()))
-print(*a)
-for i in range(1,n):
-    num=a[i]
-    j=i-1
-    while j>=0 and a[j]>num:
-        a[j+1]=a[j]
-        j-=1
-    a[j+1]=num
-    print(*a)
+def Is(a,N):
+    for i in range(1,n):
+        num=a[i]
+        j=i-1
+        while j>=0 and a[j]>num:
+            a[j+1]=a[j]
+            j-=1
+        a[j+1]=num
+    return a
