014b27998ed5f4848b9995f31e486cc8897980ee
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-07-04 00:12:13 +0900

    ?

--- Morris-Pratt.py
+++ Morris-Pratt.py
@@ -1,17 +1,11 @@
-from sys import stdin
+import sys
+input=sys.stdin.readline
 
-input=stdin.readline
-
-def morris_pratt(string,strlen,ans_array):
+def morris_pratt(st,stlen,ans_array):
     j=-1
-    for i in range(strlen):
-        while j>=0 and string[i]!=string[j]:
+    for i in range(stlen):
+        while j>=0 and st[i]!=st[j]:
             j=ans_array[j]
         j+=1
         ans_array[i+1]=j
     return ans_array
-
-s=input().rstrip()
-n=len(s)
-ans=[-1]+[0]*n
-print(*morris_pratt(s,n,ans))
