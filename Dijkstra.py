5c84808dbd2f4c570191c83bdcc90c5c395216f2
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-07-16 11:00:20 +0900

    INFが小さいのと,バグってたところ

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,26 +1,19 @@
 import heapq
-INF=1e100
 
-#G(cost,vertex)
-def dijkstra(G,start,n):#グラフ,初期頂点,頂点数
-
-    dist=[INF]*n;dist[start]=0
-    used=[False]*n;used[start]=True
-    q=[]
-
-    for v in G[start]:
-        heapq.heappush(q,v)
+INF=1e18
+def dijkstra(G,n,start):#グラフ,初期頂点,頂点数
     
-    while len(q)>0:
-        now=heapq.heappop(q)
-        if used[now[1]]:
-            continue
-        v=now[1]
-        dist[v]=now[0]
-        used[v]=True
+    dist=[INF]*n;dist[start]=0
+    q=[]
+    heapq.heappush(q,(0,start))
+    
+    while len(q):
+        now_dist,v=heapq.heappop(q)
+        if dist[v]<now_dist: continue
 
-        for e in G[v]:
-            if not used[e[1]]:
-                heapq.heappush(q,(e[0]+dist[v],e[1]))
-
+        for e1,e2 in G[v]:
+            if dist[v]+e1<dist[e2]:
+                dist[e2]=dist[v]+e1
+                heapq.heappush(q,(dist[e2],e2))
+        
     return dist
