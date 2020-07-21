5630efd9e91db3c17bbfc35031ed0c3b4a6dc204
naru2001 <50087796+naru2001@users.noreply.github.com>
2020-07-21 18:41:30 +0900

    素数リスト削除
    素因数分解

--- Prime_factorize.py
+++ Prime_factorize.py
@@ -1,12 +1,8 @@
-def Pf(n,prime=None):#nの素因数分解(prime:素数リスト)
-
-    if prime==None: prime=[True]*(n+1)
-
+def Pf(n):#nの素因数分解
     ret=[]
     tmp=n
 
     for i in range(2,int(n**0.5)+1):
-        if not prime[i]: continue
         if tmp%i==0:
             while tmp%i==0:
                 ret.append(i)
